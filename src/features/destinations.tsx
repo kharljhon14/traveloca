a628bdbaa46b2220c258a1f03e4fb9bc3b23b456
Kharl <enriquezkharl14@gmail.com>
2025-01-08 07:16:22 +0800

    [Style] Updated the destinations heading

--- src/features/destinations.tsx
+++ src/features/destinations.tsx
@@ -11,62 +11,18 @@ import { MapPin, Star } from 'lucide-react';
 export default function Destinations() {
   return (
     <div className="">
-      <div className="mb-6">
-        <h3 className="text-2xl font-semibold mb-2">Popular Destination</h3>
-        <p className="text-gray-500 text-sm">Unleash Your Wanderlust With SkyWings</p>
+      <div className="mb-6 lg:mx-24">
+        <h3 className="text-2xl font-semibold mb-2 lg:text-4xl">Popular Destination</h3>
+        <p className="text-gray-500 text-sm lg:text-base">Unleash Your Wanderlust With SkyWings</p>
       </div>
-      <div className=" mx-10">
+      <div className="mx-10">
         <Carousel
           opts={{
-            align: 'center'
+            align: 'start'
           }}
-          className="w-full"
+          className="w-auto mx-auto max-w-6xl"
         >
           <CarouselContent>
-            <CarouselItem className="md:basis-96 sm:basis-48">
-              <Card>
-                <CardHeader>
-                  <figure className="rounded-xl overflow-hidden mb-4">
-                    <img src="/images/palawan.webp" />
-                  </figure>
-                  <CardTitle>Forest Wild Life</CardTitle>
-                  <CardDescription>
-                    <div className="flex justify-between">
-                      <div className="flex items-center gap-x-1">
-                        <MapPin size={14} />
-                        <span>Philippines</span>
-                      </div>
-                      <div className="chip px-3">
-                        <Star size={12} />
-                        <span>4.7</span>
-                      </div>
-                    </div>
-                  </CardDescription>
-                </CardHeader>
-              </Card>
-            </CarouselItem>
-            <CarouselItem className="md:basis-96 sm:basis-48">
-              <Card>
-                <CardHeader>
-                  <figure className="rounded-xl overflow-hidden mb-4">
-                    <img src="/images/palawan.webp" />
-                  </figure>
-                  <CardTitle>Forest Wild Life</CardTitle>
-                  <CardDescription>
-                    <div className="flex justify-between">
-                      <div className="flex items-center gap-x-1">
-                        <MapPin size={14} />
-                        <span>Philippines</span>
-                      </div>
-                      <div className="chip px-3">
-                        <Star size={12} />
-                        <span>4.7</span>
-                      </div>
-                    </div>
-                  </CardDescription>
-                </CardHeader>
-              </Card>
-            </CarouselItem>
             <CarouselItem className="md:basis-96 sm:basis-48">
               <Card>
                 <CardHeader>
