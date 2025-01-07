36b6c0dc2db74168c1bfcb78da1ba5f16076aa9d
Kharl <enriquezkharl14@gmail.com>
2025-01-07 21:59:12 +0800

    [Feat] Partners feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Hero from './features/hero';
+import Partners from './features/partners';
 import MainLayout from './layouts/main-layout';
 
 function App() {
   return (
     <MainLayout>
       <Hero />
+      <Partners />
     </MainLayout>
   );
 }
---
+++ src/features/partners.tsx
@@ -0,0 +1,22 @@
+export default function Partners() {
+  return (
+    <div className="flex flex-col lg:flex-row justify-center items-center gap-y-8 lg:gap-y-0 lg:gap-x-16">
+      <img
+        className=" w-40"
+        src="/images/logos/airbnb.png"
+        alt="airbnb logo"
+      />
+
+      <img
+        className="w-40"
+        src="/images/logos/google.png"
+        alt="google logo"
+      />
+      <img
+        className="w-40"
+        src="/images/logos/amazon.png"
+        alt="amazon logo"
+      />
+    </div>
+  );
+}
